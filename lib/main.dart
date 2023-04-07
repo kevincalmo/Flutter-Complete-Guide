d98e067da818676a0dbe34c74626ba0f53b0c85a
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-04-07 16:59:01 -0400

    Début du projet

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+// Création du design de l'application.
+// En extends StatelessWidget, impossible d'y ajouter un état
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
+}
