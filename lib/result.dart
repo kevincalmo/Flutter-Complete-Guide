84ed91aec42126a38d5831013e7aff721ab039ad
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-04-08 16:21:18 -0400

    Création de la phrase de résultat renvoyé en fonction du score total obtenu + ajout de la fonction qui permet de recommencer le quiz

--- lib/result.dart
+++ lib/result.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  final String textOutput;
+  final int resultScore;
+  final VoidCallback resetQuiz;
 
-  Result({this.textOutput});
+  String get resultPhrase {
+    var resultText = 'You did it';
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are strange!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  Result({this.resultScore, this.resetQuiz});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(textOutput),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: resetQuiz,
+            child: Text('Reset the quiz'),
+          )
+        ],
+      ),
     );
   }
 }
