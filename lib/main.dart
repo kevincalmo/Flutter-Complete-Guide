9b2dda4482eb9fc452a1a5fcc4a6be32905021f4
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2023-04-08 16:19:26 -0400

    Création de la logique pour recommencer le quiz

--- lib/main.dart
+++ lib/main.dart
@@ -22,29 +22,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
   final questions = [
     {
-      'questionText': 'What\'s your favorite color ?',
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
-      'questionText': 'What\'s your favorite animal ?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
-      'questionText': 'What\'s your favorite meal ?',
-      'answers': ['Chicken', 'Fish', 'Egg', 'Melon']
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
 
-  void _answerQuestion() {
-    if (_questionIndex < questions.length) {
-      setState(() {
-        _questionIndex++;
-      });
-      print(_questionIndex);
-    }
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex++;
+    });
   }
 
   /**/
@@ -62,7 +83,8 @@ class _MyAppState extends State<MyApp> {
                 questionAnswerFunction: _answerQuestion,
               )
             : Result(
-                textOutput: 'You did it !',
+                resultScore: _totalScore,
+                resetQuiz: _resetQuiz,
               ),
       ),
     );
